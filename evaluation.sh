42b39ea997fd689de2938aeab5b35a9b2e8e60a4
Janek21 <jan.izquierdo@alum.esci.upf.edu>
2026-06-17 14:39:46 +0200

    rename properly

--- evaluation.sh
+++ evaluation.sh
@@ -49,14 +49,12 @@ find "$lyric_out" -type f -name "ont_*.gz"|xargs -r -P $(nproc) unpigz -df  #$(n
 #genome stays gzipped in ../data/species; the uncompressed copy in data/fasta is used instead
 
 #rename for long file names
-#Removes the prefix and sufix(minNreads (N varies)) and replaces it with nothing ('')
-rename "ont_HpreCap_0+_" "" "$lyric_out"/ont_HpreCap_0+_[DSE]RR*.gff
-rename 's/\.HiSS\.tmerge\.min[0-9]+reads\.splicing_status-all\.endSupport-all//' "$lyric_out"/*.gff
-
-#for f in "$lyric_out"/*.gff; do
-#	new=$(echo "$f" | sed -E 's/\.HiSS\.tmerge\.min[0-9]+reads\.splicing_status-all\.endSupport-all//')
-#	[ "$f" != "$new" ] && mv "$f" "$new"
-#done
+#Removes the prefix and sufix(minNreads (N varies))
+for f in "$lyric_out"/ont_HpreCap_0+_[DSE]RR*.gff; do
+    [ -e "$f" ] || continue
+    b=$(basename "$f"); b=${b#ont_HpreCap_0+_}; b=${b%%.HiSS.*}
+    mv -- "$f" "$lyric_out/$b.gff"
+done
 
 #detect number of files in folder(if 1 only, dont merge)
 shopt -s nullglob
